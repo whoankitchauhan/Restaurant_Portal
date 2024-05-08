81ece8f16464953f84f1f7135c2b2984c348bbf5
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-05-08 19:00:30 +0530

    Change

--- Main/index.php
+++ Main/index.php
@@ -65,7 +65,7 @@ mysqli_close($conn);
   <section class="showcase-area" id="showcase">
     <div class="showcase-container">
       <h1 class="main-title" id="home">Eat Right Food</h1>
-      <p class="title">Eat Healty, it is good for our health.</p>
+      <p class="title">Eat Healthy, it is good for our health.</p>
       <a href="#food-menu" class="btn btn-primary">Menu</a>
     </div>
   </section>
